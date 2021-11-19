8ebc0b11ad60dad2ecc4d6ffa789a91d6081ec33
tim <gretler.tim@gmail.com>
2021-11-19 09:28:53 +0000

    update Dockerfile

--- storage/kvpmem/update_kvdk.sh
+++ storage/kvpmem/update_kvdk.sh
@@ -7,7 +7,7 @@ git submodule update --remote --merge .
 
 mkdir -p build && cd build && rm -rf *
 
-cmake .. -DCMAKE_BUILD_TYPE=Release && make -j
+cmake .. -DCMAKE_BUILD_TYPE=Release -D CMAKE_C_COMPILER=gcc-8 -D CMAKE_CXX_COMPILER=g++-8  && make -j 
 cp libengine.so ../../../lib/libengine.so 
 mkdir -p ../../../include/kvdk/
 cp ../include/kvdk/* ../../../include/kvdk/
